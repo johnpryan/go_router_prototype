5131947f81a847d74bdad361cc8afbc2b53ec1d2
John Ryan <ryjohn@google.com>
2022-03-23 21:16:43 -0700

    unused imports

--- lib/src/route.dart
+++ lib/src/route.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:quiver/core.dart';
-
 import 'redirect.dart';
 import 'typedefs.dart';
 
--- test/tree_router_test.dart
+++ test/tree_router_test.dart
@@ -4,8 +4,6 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tree_router/tree_router.dart';
-
 void main() {
   test('', () {
 
